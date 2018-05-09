90eddbf407ab793935b771327b1af6bfef1e80af
Robbie <rduvall@caktusgroup.com>
2018-05-09 11:55:16 -0400

    11:55

---
+++ Page/POM.py
@@ -0,0 +1,102 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import Page.locators
+from selenium.webdriver.common import by
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class TopNav(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def nav_to_homepage(self):
+        self.driver.find_element_by_class_name("navbar-brand").click()
+
+    def nav_to_blog(self):
+        self.driver.find_element_by_class_name("posts").click()
+
+    def nav_to_services(self):
+        self.driver.find_element_by_class_name("service-page").click()
+
+    def nav_to_ourwork(self):
+        self.driver.find_element_by_class_name("our-work").click()
+
+    def nav_to_aboutus(self):
+        self.driver.find_element_by_class_name("about-us").click()
+
+    def nav_to_contact(self):
+        self.driver.find_element_by_class_name("contact").click()
+
+
+class ContactForm(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def first_name(self, first_name):
+        self.driver.find_element_by_name("firstname").send_keys(first_name)
+
+    def last_name(self, last_name):
+        self.driver.find_element_by_name("lastname").send_keys(last_name)
+
+    def email(self, email):
+        self.driver.find_element_by_name("email").send_keys(email)
+
+
+class Homepage(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_hero_image_button(self):
+        hero_button = self.driver.find_element_by_class_name("button")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "button")))
+        hero_button.click()
+
+    def click_featured_case_study(self):
+        self.driver.find_element_by_class_name("card-case-study").click()
+
+
+class Footer(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def quick_links_services(self):
+        # self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[1]/a""").click()
+        # getting an error on the link.click link of the following:
+        link = self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[1]/a""")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="footer"]/div/div[1]/div[2]/ul/li[1]/a""")))
+        link.click()
+
+    def quick_links_our_work(self):
+        # self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[2]/a""").click()
+        link = self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[2]/a""")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="footer"]/div/div[1]/div[2]/ul/li[2]/a""")))
+        link.click()
+
+    def quick_links_about_us(self):
+        # self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[3]/a""").click()
+        link = self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[3]/a""")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="footer"]/div/div[1]/div[2]/ul/li[3]/a""")))
+        link.click()
+
+    def quick_links_blog(self):
+        # self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[4]/a""").click()
+        link = self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[4]/a""")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="footer"]/div/div[1]/div[2]/ul/li[4]/a""")))
+        link.click()
+
+    def quick_links_careers(self):
+        # self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[3]/a""").click()
+        link = self.driver.find_element_by_xpath("""//*[@id="footer"]/div/div[1]/div[2]/ul/li[5]/a""")
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="footer"]/div/div[1]/div[2]/ul/li[5]/a""")))
+        link.click()
---
+++ testcases.py
@@ -0,0 +1,141 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+from time import sleep
+from selenium.webdriver.support.ui import Select
+# seems like I shouldn't have to import each class
+from Page.POM import TopNav
+from Page.POM import ContactForm
+from Page.POM import Homepage
+from Page.POM import Footer
+from selenium.webdriver.common import by
+
+# should find a way to make the links work for either environment if we are really going to use this
+# what would it take it take to get selenium set up on CW?
+# headless driver
+
+
+class SmokeTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome("/Users/robbie/Downloads/chromedriver")
+        cls.driver.maximize_window()
+        cls.driver.get("https://caktus:pointy@staging.caktusgroup.com")
+        cls.screen_name = datetime.now()
+        cls.topnav = TopNav(cls.driver)
+        cls.homepage = Homepage(cls.driver)
+        cls.footer = Footer(cls.driver)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def test_verify_blog_page_title(self):
+        # Navigates to the blog and verifies the title includes the word Blog
+
+        self.topnav.nav_to_blog()
+        # verify that Blog is in the title of the blog page
+        self.assertIn("Blog", self.driver.title), \
+            self.driver.get_screenshot_as_file("/Users/robbie/Desktop/auto/blog_page_title_%s.png" % self.screen_name)
+
+    def test_hero_button_loads_services_page(self):
+        # Verify that the button in the hero image loads the services page
+        self.topnav.nav_to_homepage()
+        self.homepage.click_hero_image_button()
+        # add a verification step here
+
+    def test_casestudies_cards_navigation(self):
+        # Verify that
+        self.homepage.click_featured_case_study()
+        # see if there is a way to use an assert here that knows what the featured case study is
+        print("This should be the name of the case study featured on the homepage: " + self.driver.title), \
+            self.driver.get_screenshot_as_file("/Users/robbie/Desktop/auto/test_CS_cards_nav_%s.png" % self.screen_name)
+
+    def test_count_blog_cards(self):
+        # use the page object to nav to blog page instead of find element here
+        self.topnav.nav_to_blog()
+        # click view more posts and wait(sleep)
+        self.driver.find_element_by_id("next").click()
+        sleep(3)
+        # count the number of cards
+        posts_count = self.driver.find_elements_by_class_name("card-common--image_container")
+        # verify the number of posts that display
+        assert (len(posts_count) == 12), \
+            self.driver.get_screenshot_as_file("/Users/robbie/Desktop/auto/count_blog_cards_%s.png" % self.screen_name)
+
+    # see if I can figure out how to test "Tech Talks card in resources section has most recent talk"
+
+    def test_footer_services(self):
+        # Verify that services link in footer loads correct page
+        self.footer.quick_links_services()
+        assert self.driver.title == "Web Development and Consulting Services | Caktus Group"
+        print("should be services: " + self.driver.title)
+
+    def test_footer_our_work(self):
+        # Verify that services link in footer loads correct page
+        self.footer.quick_links_our_work()
+        assert self.driver.title == "Web Development and Consulting Services | Caktus Group"
+        print("should be our work: " + self.driver.title)
+
+    def test_quick_links_about_us(self):
+        # Verify that...
+        self.footer.quick_links_about_us()
+        print("should be about us: " + self.driver.title)
+
+    def test_quick_links_blog(self):
+        # Verify that...
+        self.footer.quick_links_blog()
+        print("should be blog:" + self.driver.title)
+
+    def test_footer_careers(self):
+        # Verify that services link in footer loads correct page
+        self.footer.quick_links_careers()
+        assert self.driver.title == "Web Development and Consulting Services | Caktus Group"
+        print("should be services: " + self.driver.title)
+
+
+class ContactFormTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("/Users/robbie/Downloads/chromedriver")
+        self.driver.get("https://caktus:pointy@staging.caktusgroup.com")
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_contact_form(self):
+        # Use the POM class to navigate to contact page
+        navigation = TopNav(self.driver)
+        navigation.nav_to_contact()
+        # Use the POM class to enter text in the contact form
+        form = ContactForm(self.driver)
+        form.first_name("MRtest")
+        form.last_name("LastName")
+        form.email("example@example.com")
+
+
+class OtherTests(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("/Users/robbie/Downloads/chromedriver")
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_4(self):
+        # test that events match drop down selection
+        self.driver.get("https://caktus:pointy@staging.caktusgroup.com/events/")
+        # select an option from the drop down on events page by the visible text
+        select = Select(self.driver.find_element_by_id("event-year"))
+        select.select_by_value("2018")
+        # verify the year in the date field
+        # event_date = self.driver.find_elements_by_class_name("call-out")
+        # self.assertIn("2018", event_date)
+
+
+if __name__ == '__main__':
+    unittest.main()
